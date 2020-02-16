1cbaa5877f5f32319036c206d754be0946fa139b
nuntt <ngtuyetnu1997@gmail.com>
2020-02-17 02:34:02 +0700

    Google Map

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './views/share/homepage/homepage.component';
 import { HeaderComponent } from './views/share/header/header.component';
-import { MapComponent } from './views/map/map.component';
 
 import { AgmCoreModule } from '@agm/core';
 import { GmapComponent } from './views/gmap/gmap.component';
@@ -15,12 +15,13 @@ import { GmapComponent } from './views/gmap/gmap.component';
     AppComponent,
     HomepageComponent,
     HeaderComponent,
-    MapComponent,
     GmapComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyB9WtDhFH-LQCamapShPpZTXG9ZGCngxrc',
       libraries: ['places']
--- src/app/views/gmap/gmap.component.ts
+++ src/app/views/gmap/gmap.component.ts
@@ -1,19 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, NgZone, AfterViewInit } from '@angular/core';
+import { MapsAPILoader, MouseEvent } from '@agm/core';
 
 @Component({
   selector: 'app-gmap',
   templateUrl: './gmap.component.html',
   styleUrls: ['./gmap.component.css']
 })
-export class GmapComponent implements OnInit {
+export class GmapComponent implements OnInit, AfterViewInit {
+  latitude: number = 10.8414575;
+  longitude: number = 106.8098268;
+  zoom: number;
+  address: string;
+  private geoCoder;
 
-  latitude: number;
-  longitude: number;
-  zoom:number;
-  constructor() { }
+  @ViewChild('search', { static: false })
+  public searchElementRef: ElementRef;
+
+  constructor(
+    private mapsAPILoader: MapsAPILoader,
+    private ngZone: NgZone
+  ) { }
 
   ngOnInit() {
-    this.setCurrentLocation();
+  }
+
+  ngAfterViewInit(): void {
+    this.findAdress();
   }
 
   // Get Current Location Coordinates
@@ -22,9 +34,51 @@ export class GmapComponent implements OnInit {
       navigator.geolocation.getCurrentPosition((position) => {
         this.latitude = position.coords.latitude;
         this.longitude = position.coords.longitude;
-        this.zoom = 20;
+        this.zoom = 15;
+        this.getAddress(this.latitude, this.longitude);
       });
     }
   }
 
+
+  markerDragEnd($event: MouseEvent) {
+    this.latitude = $event.coords.lat;
+    this.longitude = $event.coords.lng;
+    this.getAddress(this.latitude, this.longitude);
+  }
+
+  getAddress(latitude, longitude) {
+    console.log(latitude +" " + longitude)
+    this.geoCoder.geocode({ 'location': { lat: latitude, lng: longitude } }, (results, status) => {
+      if (status === 'OK') {
+        if (results[0]) {
+          this.zoom = 12;
+          this.address = results[0].formatted_address;
+        } else {
+          window.alert('No results found');
+        }
+      } else {
+        window.alert('Geocoder failed due to: ' + status);
+      }
+    });
+  }
+
+  findAdress() {
+    this.mapsAPILoader.load().then(() => {
+      this.setCurrentLocation();
+      this.geoCoder = new google.maps.Geocoder;
+      let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement);
+      autocomplete.addListener("place_changed", () => {
+        this.ngZone.run(() => {
+          // some details
+          let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+          this.address = place.formatted_address;
+          //set latitude, longitude and zoom
+          this.latitude = place.geometry.location.lat();
+          this.longitude = place.geometry.location.lng();
+          this.zoom = 15;
+        });
+      });
+    });
+  }
 }
